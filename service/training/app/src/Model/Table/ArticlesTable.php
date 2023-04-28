2bb50d125474defe453c89326df208f4df809b3a
Sota <sota.sumii@gmail.com>
2023-04-28 14:21:00 +0900

    bake category

--- service/training/app/src/Model/Table/ArticlesTable.php
+++ service/training/app/src/Model/Table/ArticlesTable.php
@@ -44,6 +44,10 @@ class ArticlesTable extends Table
         $this->setPrimaryKey('id');
 
         $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Categories', [
+            'foreignKey' => 'category_id',
+        ]);
     }
 
     /**
